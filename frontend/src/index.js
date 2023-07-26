d7589fe206c67ea0cf4c56966b6a8cb1e0435c97
Musab Khan <musabkhan2003@gmail.com>
2023-07-26 14:07:38 +0300

    created Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'; // Import Provider
 import App from './App';
-import store from './store'; // Import the Redux store
+import store from '../src/redux/store'; // Import the Redux store
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ frontend/src/redux/reducers/propertyReducer.js
@@ -0,0 +1,22 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+export const propertyReducer = createReducer(
+  {},
+  {
+    propertyRequest: (state) => {
+      state.loading = true;
+    },
+    propertySuccess: (state, action) => {
+      (state.loading = false), (state.property = action.payload.property);
+      state.propertyCount = action.payload.propertyCount;
+      state.message = action.payload.message;
+    },
+    propertyFail: (state, action) => {
+      (state.loading = false), (state.property = null);
+      state.error = action.payload;
+    },
+    clearErrors: (state) => {
+      state.error = null;
+    },
+  }
+);
